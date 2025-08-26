5ede0afe1326536970e082f086242cc8a90ac4b0
Ajinkya <ajinkya.namra@techolution.com>
2025-08-26 15:50:34 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const fetchRole = async () => {
+    const res = await fetch(`http://localhost:3000/api/role/getall`, {
+      method: "GET",
+      credentials: 'include'
+    });
+    const session = await res.json();
+    console.log("Session",session)
+    // if(session){
+    //   window.location.href="http://localhost:3000/api/auth/signout?callbackUrl=https://rgw8sqjh-5173.inc1.devtunnels.ms/"
+    // }
+
+    // if (!session ||!session.user) {
+    //   window.location.href = "http://localhost:3000/api/auth/signin?callbackUrl=https://rgw8sqjh-5173.inc1.devtunnels.ms/";
+    //   return;
+    // }
+  }
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={fetchRole}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
